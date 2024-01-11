82dfb2f81c2257ec98705a8b23b4ae70319cc79f
bhuiamhe <bhuiamh@gmail.com>
2024-01-11 13:07:18 +0600

    Youtube Thumbnail added and Implement this feature

--- src/pages/Home/QuickService.jsx
+++ src/pages/Home/QuickService.jsx
@@ -3,19 +3,24 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import useScreenSize from "../../hooks/useScreenSize";
 
-import QuickServiceImg1 from "../../assets/QuickService/swiper1.jpg";
-import QuickServiceImg2 from "../../assets/QuickService/swiper2.jpg";
-import QuickServiceImg3 from "../../assets/QuickService/swiper3.jpg";
-import QuickServiceImg4 from "../../assets/QuickService/swiper4.jpg";
-import QuickServiceImg5 from "../../assets/QuickService/swiper5.jpg";
-import QuickServiceImg6 from "../../assets/QuickService/swiper6.jpg";
-import QuickServiceImg7 from "../../assets/QuickService/swiper7.jpg";
-import QuickServiceImg8 from "../../assets/QuickService/swiper8.jpg";
-import QuickServiceImg9 from "../../assets/QuickService/swiper9.jpg";
-import QuickServiceImg10 from "../../assets/QuickService/swiper10.jpg";
-import QuickServiceImg11 from "../../assets/QuickService/swiper11.jpg";
-import QuickServiceImg12 from "../../assets/QuickService/swiper12.jpg";
-import QuickServiceImg13 from "../../assets/QuickService/swiper13.jpg";
+import thumbnail1 from "../../assets/thumbnail/thambnail1.jpg";
+import thumbnail2 from "../../assets/thumbnail/thambnail2.jpg";
+import thumbnail3 from "../../assets/thumbnail/thambnail3.jpg";
+import thumbnail4 from "../../assets/thumbnail/thambnail4.jpg";
+import thumbnail5 from "../../assets/thumbnail/thambnail5.jpg";
+import thumbnail6 from "../../assets/thumbnail/thambnail6.jpg";
+import thumbnail7 from "../../assets/thumbnail/thambnail7.jpg";
+import thumbnail8 from "../../assets/thumbnail/thambnail8.jpg";
+import thumbnail9 from "../../assets/thumbnail/thambnail9.jpg";
+import thumbnail10 from "../../assets/thumbnail/thambnail10.jpg";
+import thumbnail11 from "../../assets/thumbnail/thambnail11.jpg";
+import thumbnail12 from "../../assets/thumbnail/thambnail12.jpg";
+import thumbnail13 from "../../assets/thumbnail/thambnail13.jpg";
+import thumbnail14 from "../../assets/thumbnail/thambnail14.jpg";
+import thumbnail15 from "../../assets/thumbnail/thambnail15.jpg";
+import thumbnail16 from "../../assets/thumbnail/thambnail16.jpg";
+import thumbnail17 from "../../assets/thumbnail/thambnail17.jpg";
+
 import SectionTitle from "../Shared/SectionTitle";
 import { Link } from "react-router-dom";
 import { BsPlayBtnFill } from "react-icons/bs";
@@ -27,7 +32,7 @@ const QuickService = () => {
   const [slides, setSlides] = useState(3);
   useEffect(() => {
     const newSlidesPerView = {
-      small: 3,
+      small: 2.5,
       medium: 4,
       large: 5,
     }[screenSize];
@@ -46,49 +51,6 @@ const QuickService = () => {
         slidesPerView={slides}
         longSwipes={true}
       >
-        {/* <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-          <h1 className="-mt-8 font-bold text-white text-center cursor-vertical-text">
-            দলিল আছে কিন্তু রেকর্ড অন্যের নামে
-          </h1>
-        </SwiperSlide> */}
-        {/* 
-        <SwiperSlide>
-          <img src={QuickServiceImg1} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg2} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg3} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg4} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg5} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg6} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg7} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg8} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg9} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg10} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg11} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg12} className="rounded-t-xl w-full" />
-        </SwiperSlide> */}
         <SwiperSlide className="relative">
           <Link
             to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
@@ -97,103 +59,167 @@ const QuickService = () => {
           >
             <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
           </Link>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+          <img src={thumbnail1} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail2} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail3} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail4} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail5} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail6} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail7} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail8} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail9} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail10} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail11} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail12} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail13} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail14} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail15} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail16} className="rounded-t-xl w-full" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={QuickServiceImg13} className="rounded-t-xl w-full" />
+        <SwiperSlide className="relative">
+          <Link
+            to="https://youtu.be/gUJvd2IWn0k?si=X8DZyDS5iOIIFjbe"
+            target="_blank"
+            className="absolute bottom-0 right-0"
+          >
+            <CiYoutube className="text-xl md:text-5xl hover:text-orange-700 text-orange-500 font-bold" />
+          </Link>
+          <img src={thumbnail17} className="rounded-t-xl w-full" />
         </SwiperSlide>
       </Swiper>
     </section>

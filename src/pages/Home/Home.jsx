e93aa2b3469b5ecd9a00239d3da8ba5b712358bf
bhuiamh <bhuiamh@gmail.com>
2024-06-30 14:08:56 +0600

    Navbar Revised Done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,20 +5,14 @@ import Login from "../Login/Login";
 import AuthorTalk from "./AuthorTalk/AuthorTalk";
 import Services from "../Services/Services";
 import AboutUs from "../AboutUs/AboutUs";
-import Modal from "./Modal/Modal";
 import FAQ from "./Faq/Faq";
 import ClientSwiper from "./ClientSwiper/ClientSwiper";
 
 const Home = () => {
-  const [showModal, setShowModal] = useState(true);
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
+  
 
   return (
     <div className="max-w-[1440px]">
-      <div>{showModal && <Modal onClose={handleCloseModal} />}</div>
       <Banner />
       <AuthorTalk />
       <Services />
--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import logo from "../../assets/shohozain.png";
-import { CgProfile } from "react-icons/cg";
-import { CiSettings } from "react-icons/ci";
 import { AiOutlineLogout } from "react-icons/ai";
-import { TiThMenu } from "react-icons/ti";
-import { RiHome2Fill, RiLiveLine } from "react-icons/ri";
+import { CgProfile } from "react-icons/cg";
+import { CiDark, CiLight, CiSettings } from "react-icons/ci";
+import { GoLaw } from "react-icons/go";
 import { LiaHandsHelpingSolid } from "react-icons/lia";
 import { MdConnectWithoutContact } from "react-icons/md";
-import { GoLaw } from "react-icons/go";
+import { RiHome2Fill, RiLiveLine } from "react-icons/ri";
+import { TiThMenu } from "react-icons/ti";
+
+import { Link } from "react-router-dom";
+import logo from "../../assets/shohozain.png";
 
 const Navbar = () => {
+  const user = true;
   // Theme Changer
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "halloween"
@@ -28,12 +30,6 @@ const Navbar = () => {
     } else {
       setTheme("halloween");
     }
-  };
-
-  // Menu Handler
-  const [menuOpen, setMenuOpen] = useState();
-  const handleMenu = () => {
-    setMenuOpen(!menuOpen);
   };
 
   const menuItem = (
@@ -62,8 +58,6 @@ const Navbar = () => {
     </>
   );
 
-  console.log(menuOpen, "from menuOpen");
-
   return (
     <div className="w-full flex justify-center fixed z-10 bg-base-300/50 backdrop-blur-md">
       <div className="w-full flex items-center p-0 px-4 tablet:px-0 max-w-[1280px]">
@@ -73,12 +67,9 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="flex-auto">
-          <div className="tablet:hidden inline-block">
-            <details className="dropdown">
-              <summary
-                onClick={handleMenu}
-                className={`${menuOpen ? "rotate-90" : ""} m-1 btn btn-ghost`}
-              >
+          <div role="button" className="tablet:hidden inline-block dropdown">
+            <details>
+              <summary className="m-1 btn btn-ghost">
                 <TiThMenu className="text-xl text-orange-500" />
               </summary>
               <ul className="p-5 gap-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
@@ -86,16 +77,24 @@ const Navbar = () => {
               </ul>
             </details>
           </div>
-          <div className="hidden tablet:inline-block ">
+          <div className="hidden tablet:inline-block">
             <div className="flex gap-4 text-[18px]">{menuItem}</div>
           </div>
         </div>
-        <div className="flex-none">
-          <div className="dropdown dropdown-end">
+        {!user && (
+          <Link
+            to="login"
+            className="text-orange-500 hover:bg-orange-500 hover:text-white duration-300 font-bold px-3 py-1 border border-orange-500 rounded"
+          >
+            লগ ইন
+          </Link>
+        )}
+        {user && (
+          <div className="dropdown dropdown-end ">
             <div
               tabIndex={0}
               role="button"
-              className="btn btn-ghost btn-circle avatar"
+              className="btn btn-ghost btn-circle avatar  border border-orange-500"
             >
               <div className="w-10 rounded-full">
                 <img
@@ -110,7 +109,8 @@ const Navbar = () => {
             >
               <li>
                 <Link className="font-semibold text-orange-500 hover:text-orange-700">
-                  <CgProfile /> প্রোফাইল
+                  <CgProfile />
+                  প্রোফাইল
                 </Link>
               </li>
               <li>
@@ -123,31 +123,25 @@ const Navbar = () => {
                   <AiOutlineLogout /> লগ আউট
                 </Link>
               </li>
+              <li
+                onClick={handleTheme}
+                className="mt-2 font-semibold text-orange-500 hover:text-orange-700"
+              >
+                {theme == "halloween" && (
+                  <h1 className="flex">
+                    {" "}
+                    <CiLight /> লাইট মুড
+                  </h1>
+                )}
+                {theme == "retro" && (
+                  <h1 className="flex">
+                    <CiDark /> ডার্ক মুড
+                  </h1>
+                )}
+              </li>
             </ul>
           </div>
-        </div>
-        <div className="ml-2">
-          <div onClick={handleTheme}>
-            {theme == "halloween" && (
-              <svg
-                className="swap-on text-orange-500 fill-current w-10 h-10"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-              </svg>
-            )}
-            {theme == "retro" && (
-              <svg
-                className="swap-off text-orange-500 fill-current w-8 h-8"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-              </svg>
-            )}
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
